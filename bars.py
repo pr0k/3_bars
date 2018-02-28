1ca44b57dc4d8ace4745c08379ee302573de6a98
pr01k <pr01k@yandex.ru>
2018-02-28 16:22:48 +0300

    fixed code accordance with PEP8

--- bars.py
+++ bars.py
@@ -8,25 +8,36 @@ def load_data(filepath):
 
 
 def get_biggest_bar(python_object):
-    return max(python_object['features'], key=lambda item: item['properties']['Attributes']['SeatsCount'])
+    return max(python_object['features'], key=lambda item:
+               item['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar(python_object):
-    return min(python_object['features'], key=lambda item: item['properties']['Attributes']['SeatsCount'])
+    return min(python_object['features'], key=lambda item:
+               item['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(python_object, longitude, latitude):
     return min(python_object['features'], key=lambda item:
-    [abs(item['geometry']['coordinates'][0] - float(longitude)), abs(item['geometry']['coordinates'][1] - float(latitude))])
+               [abs(item['geometry']['coordinates'][0] - float(longitude)),
+               abs(item['geometry']['coordinates'][1] - float(latitude))])
 
 
 if __name__ == '__main__':
     try:
         python_obj_data = load_data(argv[1])
-        print('\n', 'The biggest bar'.center(36, '*'), '\n\n', json.dumps(get_biggest_bar(python_obj_data), ensure_ascii=False, indent=4))
-        print('\n' ,'The smallest bar'.center(36, '*'), '\n\n', json.dumps(get_smallest_bar(python_obj_data), ensure_ascii=False, indent=4))
-        print('\n' ,'The closest bar'.center(36, '*'), '\n\n', json.dumps(get_closest_bar(python_obj_data, argv[2], argv[3]), ensure_ascii=False, indent=4))
+        print('\n', 'The biggest bar'.center(36, '*'), '\n\n',
+              json.dumps(get_biggest_bar(python_obj_data), ensure_ascii=False,
+              indent=4))
+        print('\n', 'The smallest bar'.center(36, '*'), '\n\n',
+              json.dumps(get_smallest_bar(python_obj_data), ensure_ascii=False,
+              indent=4))
+        print('\n', 'The closest bar'.center(36, '*'), '\n\n', json.dumps(
+            get_closest_bar(python_obj_data, argv[2], argv[3]),
+            ensure_ascii=False, indent=4))
     except ValueError:
-        print('ERROR:\nthere is no JSON data in the file {0}\nspecify the JSON data file'.format(argv[1]))
+        print('ERROR:\nthere is no JSON data in the file {0}\nspecify the\
+        JSON data file'.format(argv[1]))
     except IndexError:
-        print('ERROR:\nnot enough arguments\ntry $ python bars.py <path to json file> <longitude> <latitude>')
+        print('ERROR:\nnot enough arguments\ntry $ python bars.py <path to\
+        json file> <longitude> <latitude>')
